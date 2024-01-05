6c187ef76ac7cbe6d40bc274e1e674e7d80cb7ac
joseunghee <sa02045@naver.com>
2024-01-05 22:38:27 +0900

    Add: JSX type

--- src/outside-click-element-define.ts
+++ src/outside-click-element-define.ts
@@ -4,6 +4,24 @@ const root = (
   typeof globalThis !== 'undefined' ? globalThis : window
 ) as typeof window;
 
+type JSXBase = JSX.IntrinsicElements extends { span: unknown }
+  ? JSX.IntrinsicElements
+  : Record<string, Record<string, unknown>>;
+declare global {
+  interface Window {
+    OutsideClickElement: typeof OutsideClickElement;
+  }
+  interface HTMLElementTagNameMap {
+    'outside-click': OutsideClickElement;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      ['outside-click']: JSXBase['div'] &
+        Partial<Omit<OutsideClickElement, keyof HTMLElement>>;
+    }
+  }
+}
+
 try {
   root.OutsideClickElement = OutsideClickElement.define();
 } catch (e: unknown) {
